b772320e1d2b514d14579cecf4bb65a9a4551319
Umar Inuwa Hassan <92232957+Umarfaroouq@users.noreply.github.com>
2024-02-28 09:37:35 +0100

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var btc=document.getElementById("bitcoin");
-var eth =document.getElementById("ethereum");
-var doge=document.getElementById("dogecoin");
-var settings={
-    "async":true,
-    "scrossDomain":true,
-    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-    "method": "GET",
-    "headers":{}
-}
-$.ajax(settings).done(function(response){
-   btc.innerHTML=response.bitcoin.usd;
-   eth.innerHTML=response.ethereum.usd;
-   doge.innerHTML=response.dogecoin.usd;
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
 });
+
+// Select elements for Bitcoin, Ethereum, and Dogecoin prices
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
+
+// API request settings
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2
